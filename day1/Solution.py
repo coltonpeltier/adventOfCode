7cd026eb40610dba0076d6c6cd5401759e53bd7c
SESA214763 <colton.peltier@schneider-electric.com>
2015-12-08 03:05:12 -0600

    finish day 1

--- day1/Solution.py
+++ day1/Solution.py
@@ -5,8 +5,14 @@ possibilities['('] = 1
 possibilities[')'] = -1
 possibilities['\n'] = 0
 floor = 0
+count = 0
+hasEnteredBasement = False
 
 for myChar in myData:
+	count += 1
 	floor += possibilities[myChar]
+	if (floor == -1 and hasEnteredBasement == False):
+		print("First entered basement at position " + str(count))
+		hasEnteredBasement = True
 
 print(floor)	
